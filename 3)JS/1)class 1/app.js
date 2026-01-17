12501f9a3909f5a292d4897734a0d74b1d2a0b84
muhammadtaharauf12 <muhammadtaharauf12@email.com>
2026-01-17 15:36:29 +0500

    teach js from chapter 1 to 4

---
+++ 3)JS/1)class 1/app.js
@@ -0,0 +1,62 @@
+// alert("Hello World " );
+//  alert("hello world 2")
+
+
+// var name1;              //declaration
+// name1 = "hamza"         //initialization
+
+
+
+
+//  var name2 = "hello world"  //declaration + initialization
+
+
+
+
+//  var msg = "Hello World";
+//  alert(msg)
+
+
+// var weight = 150;
+
+
+
+
+// var orginalNum = 150;
+// var sum = orginalNum+3  //28
+// console.log(sum)
+
+
+// var a = 5;
+// var b = 6;
+// var c = a+b;
+// console.log(c);
+
+
+// var orginalNum = 90;
+// orginalNum = orginalNum +10;
+
+// console.log(orginalNum);
+
+
+//  var originalNum = "23"; 
+//  var newNum = originalNum - 7;   //Concatination (jb bhi strng k sath numb add(+) hojae)
+//  console.log(newNum);
+
+
+
+// Rules of variables
+
+// var 1num    wrong
+// var num1    correct
+ 
+
+// var Name1;
+// var $Hello_123  
+
+// var rose = "flower"
+// var Rose ="rose"
+
+// console.log(Rose);
+
+
